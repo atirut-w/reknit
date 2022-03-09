85ea0802e15193964d79e16ba56838c4d2dd531c
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-09 22:23:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Reknit
+A SysVinit-based init system for the Cynosure 2 kernel
+
+# Using Reknit
+Simply copy `init.lua` into anywhere on your filesystem that have Cynosure 2 installed, preferably in `/sbin/init.lua`, and pass the parameter `init=path/to/init.lua` to the kernel.
+
+Example configuration for CLDR:
+```cfg
+flags init=path/to/init.lua
+```
+
+## `inittab` file
+Reknit use almost the exact same format as SysVinit. Each line is a service, and each service is a list of parameters.
+
+Service syntax:
+```
+id:runlevels:action:path
+```
+
+Example:
+```
+shell:0:respawn:/bin/sh.lua
+```
+
+- `id` can be however long you want. At the moment, it is not used.
+- `runlevels` is a number where each digit indicates at what level the action should be executed. Again, not used at the moment.
+- `action` defines the behaviour of the service. At the moment, 3 actions are supported:
+  - `once`: the service will be executed only once without Reknit waiting for it to finish.
+  - `wait`: the service will be executed and Reknit will wait for it to finish.
+  - `respawn`: the service will be executed and Reknit will respawn it if it dies.
---
+++ init.lua
@@ -0,0 +1,106 @@
+--!lua
+-- System V-style init
+
+--- Perform a system call
+---@param call string
+---@vararg any
+local function syscall(call, ...)
+  return coroutine.yield("syscall", call, ...)
+end
+
+---@param fmt string
+---@vararg any
+local function printf(fmt, ...)
+  syscall("write", 1, string.format(fmt, ...))
+end
+
+--- Fork a new process and execute an executable
+---@param path string
+---@return integer
+local function exec(path)
+  local pid = syscall("fork", function()
+    local _, errno = syscall("execve", path, {}, {})
+    if errno then
+      printf("Could not execute %s: %d\n", path, errno)
+    end
+  end)
+  return pid
+end
+
+---@class InitEntry
+---@field id string
+---@field runlevels integer[]
+---@field action string
+---@field command string
+
+---@class WatchEntry
+---@field entry InitEntry
+---@field pid integer
+
+---@type InitEntry[]
+local init_entries = {}
+---@type WatchEntry[]
+local watchlist = {}
+
+do
+  local fd, errno = syscall("open", "/etc/inittab", "r")
+  if not fd then
+    printf("Failed to open /etc/inittab: %s\n", errno)
+    syscall("exit", 1)
+  end
+  ---@type string
+  local content = syscall("read", fd, "a")
+  syscall("close", fd)
+
+  for line in content:gmatch("[^\r\n]+") do
+    if line:sub(1, 1) == ":" then goto continue end
+
+    local id, runlevels, action, command = line:match("^([^:]+):([^:]+):([^:]+):(.+)$")
+    if id and runlevels and action and command then
+      local entry = {
+        id = id,
+        runlevels = {},
+        action = action,
+        command = command
+      }
+      for runlevel in runlevels:gmatch("%d+") do
+        entry.runlevels[runlevel] = true
+      end
+      table.insert(init_entries, entry)
+    end
+
+    ::continue::
+  end
+end
+
+for _, entry in ipairs(init_entries) do
+  if entry.action == "once" then
+    exec(entry.command)
+  elseif entry.action == "wait" then
+    syscall("wait", exec(entry.command))
+  elseif entry.action == "respawn" then
+    local watch = {
+      entry = entry,
+      pid = exec(entry.command)
+    }
+    table.insert(watchlist, watch)
+  else
+    printf("Unknown action for '%s': %s\n", entry.id, entry.action)
+    goto continue
+  end
+
+  ::continue::
+end
+
+while true do
+  local sig, pid = coroutine.yield(0)
+
+  if sig == "process_exit" then
+    for _, watch in ipairs(watchlist) do
+      if watch.pid == pid then
+        watch.pid = exec(watch.entry.command)
+        break
+      end
+    end
+  end
+end
